a968fee30240785e8c396a0171faaa52a17912d6
vinicius valle <valleconexoes@gmail.com>
2021-11-13 16:35:57 -0300

    botão da ação slap

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :pingochis, only: [:new, :create, :show] do
     patch 'slap', to: 'pingochis#slap'
+    #acrescentar aqui as próximas ações
   end
 end
