7cece026da1fd67bff9314e75e3bcf694efd70fd
Vinicius Marques <v.alvesmarques@gmail.com>
2021-11-14 10:50:16 -0300

    Fix UTI logic and reinstall sweet alert 2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -24,6 +25,8 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import Swal from 'sweetalert2'
+window.Swal = Swal;
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
