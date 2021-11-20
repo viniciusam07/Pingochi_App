7fea0e719277e9b4ebd23f2018d8c157aabd562e
Bianca Alves <biamatos04@gmail.com>
2021-11-20 11:41:08 -0300

    penguin itens

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,29 @@ categories = Category::TYPES.map do |name|
 end
 
 categories.each do |category|
-  Item.create!(category: category, description: "Iglu pequeno", name:"Iglu")
+  Item.create!(category: category, description: "Little Igloo", name:"Igloo")
+end
+
+categories.each do |category|
+  Item.create!(category: category, description: "Scarf blue", name:"Scarf")
+end
+
+categories.each do |category|
+  Item.create!(category: category, description: "Fishing rod black", name:"Fishing rod")
+end
+
+categories.each do |category|
+  Item.create!(category: category, description: "Shirt", name:"Clothes")
+end
+
+categories.each do |category|
+  Item.create!(category: category, description: "Blue Sneakers", name:"Shoes")
+end
+
+categories.each do |category|
+  Item.create!(category: category, description: "Red hat", name:"Hat")
+end
+
+categories.each do |category|
+  Item.create!(category: category, description: "Fish", name:"Food")
 end
