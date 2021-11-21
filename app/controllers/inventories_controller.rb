b1d218930d20a8bb6363b708440edfeb69950291
Vinicius Marques <v.alvesmarques@gmail.com>
2021-11-21 18:54:28 -0300

    Added Wallet amount X Item Price comparison

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -3,9 +3,14 @@ class InventoriesController < ApplicationController
   def create
     pingochi = Pingochi.find(params[:pingochi_id])
     item = Item.find(params[:inventory][:item_id])
-    # Alerta de comparar saldo wallet com valor do item
-    Inventory.create!(user: current_user, pingochi: pingochi, item: item)
-    current_user.wallet.update!(fishcoin_amount: current_user.wallet.fishcoin_amount - item.value)
-    redirect_to pingochi_items_path(pingochi)
+    if item.value <= current_user.wallet.fishcoin_amount
+      Inventory.create!(user: current_user, pingochi: pingochi, item: item)
+      current_user.wallet.update!(fishcoin_amount: current_user.wallet.fishcoin_amount - item.value)
+      redirect_to pingochi_items_path(pingochi)
+      flash[:notice] = "You bought #{item.name}!!!"
+    else
+      redirect_to pingochi_items_path(pingochi)
+      flash[:notice] = "Your fishcoin balance it is not enough "
+    end
   end
 end
