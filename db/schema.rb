67e0bde0a045b026df5658600dfe8235f52195dd
vinicius valle <valleconexoes@gmail.com>
2021-11-23 21:13:40 -0300

    add banner to home

--- db/schema.rb
+++ db/schema.rb
@@ -42,14 +42,6 @@ ActiveRecord::Schema.define(version: 2021_11_23_141904) do
     t.index ["category_id"], name: "index_items_on_category_id"
   end
 
-  create_table "pingochi_friendships", force: :cascade do |t|
-    t.bigint "pingochi_id", null: false
-    t.integer "pingochi_friend_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["pingochi_id"], name: "index_pingochi_friendships_on_pingochi_id"
-  end
-
   create_table "pingochis", force: :cascade do |t|
     t.string "name", null: false
     t.boolean "live", default: true, null: false
@@ -61,8 +53,8 @@ ActiveRecord::Schema.define(version: 2021_11_23_141904) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "slept_at"
-    t.datetime "uti_at"
     t.string "specie"
+    t.datetime "uti_at"
     t.bigint "item_id"
     t.index ["item_id"], name: "index_pingochis_on_item_id"
     t.index ["user_id"], name: "index_pingochis_on_user_id"
@@ -93,7 +85,6 @@ ActiveRecord::Schema.define(version: 2021_11_23_141904) do
   add_foreign_key "inventories", "pingochis"
   add_foreign_key "inventories", "users"
   add_foreign_key "items", "categories"
-  add_foreign_key "pingochi_friendships", "pingochis"
   add_foreign_key "pingochis", "items"
   add_foreign_key "pingochis", "users"
   add_foreign_key "wallets", "users"
