f462992431aae9577810931a20f49bcac72071af
Vinicius Marques <v.alvesmarques@gmail.com>
2021-11-27 15:12:08 -0300

    Add Battle and Opponents features

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -1,3 +1,28 @@
 class BattlesController < ApplicationController
 
+  def new
+    @pingochi1 = Pingochi.find(params[:pingochi_id])
+    @pingochi2 = Pingochi.find(params[:opponent_id])
+    @battle = Battle.new
+  end
+
+  def create
+    @pingochi1 = Pingochi.find(params[:pingochi_id])
+    @pingochi2 = Pingochi.find(params[:opponent_id])
+    pingochi_winner = winner(@pingochi1, @pingochi2)
+    @battle = Battle.new(pingochi1: @pingochi1, pingochi2: @pingochi2, pingochi_winner: pingochi_winner)
+    if @battle.save
+      render 'show'
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @battle =Battle.find(params[:id])
+  end
+
+  def winner(pingochi1, pingochi2)
+    @pingochi1
+  end
 end
