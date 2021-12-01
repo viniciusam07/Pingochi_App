6727c3984a7073b2b61d6bf81c61eb0516e36b86
Vinicius Marques <v.alvesmarques@gmail.com>
2021-12-01 11:29:45 -0300

    Add logic to battle and mating badges on Pingochi show View

--- app/controllers/matings_controller.rb
+++ app/controllers/matings_controller.rb
@@ -25,10 +25,11 @@ class MatingsController < ApplicationController
   def mating_process(pingochi1, pingochi2)
     @pingochi1_xp = pingochi1.strength_skill + pingochi1.speed_skill + pingochi1.inteligence_skill
     @pingochi2_xp = pingochi2.strength_skill + pingochi2.speed_skill + pingochi2.inteligence_skill
-    if @pingochi1_xp + @pingochi2_xp >= 10
-      @mating_status == true
+    @couple_xp = @pingochi1_xp + @pingochi2_xp
+    if @couple_xp > 10
+      @mating_status = true
     else
-      @mating_status == false
+      @mating_status = false
     end
   end
 end
