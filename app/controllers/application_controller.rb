47987fd6e3dc13e45f508030b77dd1dc5360ae95
Bianca Alves <biamatos04@gmail.com>
2021-11-30 21:30:48 -0300

    photo-correction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :photo])
   end
 end
