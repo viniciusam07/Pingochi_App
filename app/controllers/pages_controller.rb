2b4b91352dd1ad783f1890e54445f766bf56347a
vinicius valle <valleconexoes@gmail.com>
2021-12-02 21:54:44 -0300

    adding counters

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @users = User.count #variável instância controller p/ view
+    @total_fishcoins = 0
+    Wallet.all.each do |wallet|
+      @total_fishcoins += wallet.fishcoin_amount
+    end
+    @fishcoins_to_currency = @total_fishcoins * 17.35
   end
 end
