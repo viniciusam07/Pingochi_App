63c672496f29e361ceb01ac14f2a43f66275675f
Vinicius Marques <v.alvesmarques@gmail.com>
2021-12-07 21:18:14 -0300

    Add music to battle

--- app/javascript/packs/fight.js
+++ app/javascript/packs/fight.js
@@ -5,12 +5,13 @@ const fight = () => {
     const new_battle = document.getElementById("new-battle");
     const fight = document.getElementById("fight");
     const create_battle_btn = document.getElementById("create-battle-btn");
-
+    const audio_battle = document.getElementById("audio-battle");
     btn_fight.addEventListener("click", () => {
       new_battle.classList.add("d-none");
       fight.classList.remove("d-none");
       btn_fight.classList.add("d-none");
-
+      audio_battle.volume = 0.5;
+      audio_battle.play();
       setTimeout(() => {
         create_battle_btn.click();
       }, 10000);
